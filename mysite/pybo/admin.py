9dc84e442eeece6c9fc00d2944f540d5a0d48bbd
dongseoki <43495319+dongseoki@users.noreply.github.com>
2022-04-18 07:46:57 +0900

    test: test admin feature(02-03 admin)

--- mysite/pybo/admin.py
+++ mysite/pybo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question
+# from .models import *
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+admin.site.register(Question)
+# admin.site.register(Answer)
