746d1d90d40398bb0042676b51bd6935942c12c8
dongseoki <43495319+dongseoki@users.noreply.github.com>
2022-05-16 07:49:00 +0900

    refactor: postgre 접속 정보 추가.

--- mysite/config/settings/prod.py
+++ mysite/config/settings/prod.py
@@ -1,3 +1,4 @@
+import environ
 from .base import *
 
 ALLOWED_HOSTS = ['3.39.142.146','pybods.shop']
@@ -5,3 +6,17 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 
 DEBUG = False
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}
